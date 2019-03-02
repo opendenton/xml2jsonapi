3a96831cec4e15d0176af8cb38036c38b318950e
Kyle Taylor <kyletaylored@gmail.com>
2019-03-02 16:11:51 -0600

    Remove all node modules

--- app.js
+++ app.js
@@ -1,41 +1,28 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const port = process.env.PORT || 3000;
+const express = require("express");
+const convert = require("xml-js");
+const fetch = require("node-fetch");
 
 var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
-app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-// app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.listen(port, err => {
+  if (err) throw err;
+  console.log(`> Ready On Server http://localhost:${port}`);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+app.get("/", function(req, res, next) {
+  console.log(req);
+  if (req.query && req.query.q) {
+    let compact = req.query.compact ? true : false;
+    fetch(req.query.q)
+      .then(resp => resp.text())
+      .then(function(data) {
+        res.setHeader("Content-Type", "application/json");
+        res.send(data);
+      })
+      .catch(err => console.log(err));
+  } else {
+    res.render("index", { title: "Express" });
+  }
 });
-
-module.exports = app;
